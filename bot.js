1c28ea3a349f0177fab83f02a79c60ca56590e43
hamo21 <40078281+hamo21@users.noreply.github.com>
2018-06-18 14:26:24 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -139,9 +139,9 @@ client.on("message", message => {
      let args = message.content.split(" ").slice(1);
        var nam = args.join(' ');
     
-      if(!message.member.hasPermission('ADMINISTRATOR')) return    message.channel.send('`ADMINISTRATOR` للأسف هذه الخاصية تحتاج الى ').then(msg => msg.delete(6000))
+      if(!message.member.hasPermission('ADMINISTRATOR')) return    message.channel.send('`manager Roles` للأسف هذه الخاصية تحتاج الى ').then(msg => msg.delete(6000))
       if (!nam) return message.channel.send(`<@${message.author.id}> يجب عليك ادخال اسم`).then(msg => msg.delete(10000))
-      message.guild.createChannel(nam, 'voice').then(c => setTimeout(() => c.delete(), 200000)) // كل 60 تساوي دقيقة عدل عليها الوقت لي تبيه 
+      message.guild.createChannel(nam, 'voice').then(c => setTimeout(() => c.delete(), 300000)) // كل 60 تساوي دقيقة عدل عليها الوقت لي تبيه 
       message.channel.send(`:ballot_box_with_check: TemporarySound : \`${nam}\``).then(c => setTimeout(() => c.edit(`<@${message.author.id}> :stopwatch:  انتهى وقت الروم الصوتي`), 200000))  // 120000 دقيقتان
     }
     });
