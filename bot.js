9991330250097e3d5c8c32623a27a77cbff613e6
hamo21 <40078281+hamo21@users.noreply.github.com>
2018-06-18 04:18:56 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -14,8 +14,6 @@ client.on('message', message => {
 
 
 
-const Discord = require ("discord.js");
-const client = new Discord.Client();
     
 client.on('message', message => {
      if (message.content === "رابط") {
@@ -30,8 +28,6 @@ https://discord.gg/2UJV6Hp
 
 
 
-
-client.login("NDU1NTYwMjI1MjQ4OTAzMTY4.DgGeEA.2-OYyC-CB9j9NksGR_uf4OqIXP0")
 client.on('ready', () => {
    console.log(`----------------`);
       console.log(`Desert Bot- Script By : i1Suhaib`);
