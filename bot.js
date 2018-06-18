24d76f3e3a1624eea9e3333ebe3060b8384f6c62
hamo21 <40078281+hamo21@users.noreply.github.com>
2018-06-18 06:17:50 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -11,6 +11,24 @@ client.on('message', message => {
       }
 });
 
+
+const Discord = require("discord.js");
+const client = new Discord.Client();
+var prefix = "@";
+client.on("message", message => {
+
+            if (message.content.startsWith(prefix + "bc")) {
+                         if (!message.member.hasPermission("ADMINISTRATOR"))  return;
+  let args = message.content.split(" ").slice(1);
+  var argresult = args.join(' '); 
+  message.guild.members.filter(m => m.presence.status !== 'offline').forEach(m => {
+ m.send(`${argresult}\n ${m}`);
+})
+ message.channel.send(`\`${message.guild.members.filter(m => m.presence.status !== 'online').size}\` : عدد الاعضاء المستلمين`); 
+ message.delete(); 
+};     
+});
+
 client.on('message', msg => {
   if (msg.content === 'رابط') {
     msg.reply('**تم ارسال الرابط في الخاص**');
